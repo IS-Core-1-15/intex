b828535614f6af096197dd12a341ec6cc43668c6
Kalvin Wettstein <kalvinwettstein@gmail.com>
2021-12-01 19:59:36 -0700

    Saftey updates

--- OpiatePrescriptions/settings.py
+++ OpiatePrescriptions/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 from pathlib import Path
+from getpass import getpass
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,8 +78,11 @@ WSGI_APPLICATION = 'OpiatePrescriptions.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'crud_movie',
+        'USER': 'postgres',
+        'PASSWORD': getpass(), #this will ask you to enter your local DB password when you start the server or run migrations
+        'HOST': 'localhost'
     }
 }
 
