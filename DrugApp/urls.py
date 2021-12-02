997c33a1c9691ed62c6f277c84bcffa04f488765
Kalvin Wettstein <kalvinwettstein@gmail.com>
2021-12-02 14:06:46 -0700

    search form

--- DrugApp/urls.py
+++ DrugApp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import indexPageView
+from .views import indexPageView, search
 
 urlpatterns = [
     path("", indexPageView, name="index"),
+    path("", search, name='search')
 ]
--- DrugApp/views.py
+++ DrugApp/views.py
@@ -3,4 +3,14 @@ from .models import *
 
 # Create your views here.
 def indexPageView(request):
-    return render(request, 'DrugApp/index.html')
+    return render(request, 'DrugApp/search.html')
+
+def search(request):
+    key = request.POST['key']
+    print(key)
+    q = f"select fname, lname from pd_prescriber where fname like '%{key}%';"
+    prescibers = PdPrescriber.objects.raw(q)
+    context = {
+        'data': prescibers,
+    }
+    return render(request, 'DrugApp/search.html', context)
