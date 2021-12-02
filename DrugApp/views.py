bf59bbb12d605e1a2eaede90506f352fc77a4112
Kalvin Wettstein <kalvinwettstein@gmail.com>
2021-12-02 15:46:08 -0700

    search function works

--- DrugApp/views.py
+++ DrugApp/views.py
@@ -9,21 +9,26 @@ def indexPageView(request):
 
 
 def searchPageView(request):
-    print(request.method)
     if request.method == 'POST':
         # get the search key
         key = request.POST['key'].title()
 
-        prescribers = PdPrescriber.objects.filter(
-            fname__contains=key) | PdPrescriber.objects.filter(lname__contains=key)
-        if len(prescribers) > 0:
-            msg = f'We found {len(prescribers)} results'
+        if request.POST['choice'] == 'Prescriber':
+            data = PdPrescriber.objects.filter(
+                fname__contains=key) | PdPrescriber.objects.filter(lname__contains=key)
+        else:
+            data = PdDrugs.objects.filter(
+                drugname__contains=key)
+
+        if len(data) > 0:
+            msg = f'We found {len(data)} results'
         else:
             msg = f'Sorry we could not find that'
         context = {
-            'data': prescribers,
+            'data': data,
             'msg': msg,
         }
+
         return render(request, 'DrugApp/search.html', context)
     else:
         return render(request, 'DrugApp/search.html')
