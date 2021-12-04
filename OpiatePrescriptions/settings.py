eff9085294a86b5d6fa42aa1d844aee05d9dce21
zuuab <zacharymartinheaton@gmail.com>
2021-12-04 11:38:31 -0700

    documentation on html and fixed collectstatic

--- OpiatePrescriptions/settings.py
+++ OpiatePrescriptions/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'OpiatePrescriptions/static')
+    os.path.join(BASE_DIR, 'DrugApp/static')
 ]
 
 # Default primary key field type
