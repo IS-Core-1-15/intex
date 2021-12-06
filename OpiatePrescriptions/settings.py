23089ba5fbc06722f3ac4ed7ac65661781c69221
Kalvin Wettstein <kalvinwettstein@gmail.com>
2021-12-06 11:47:56 -0700

    db stuff

--- OpiatePrescriptions/settings.py
+++ OpiatePrescriptions/settings.py
@@ -109,8 +109,8 @@ else:
             'HOST': 'localhost'
         }
     }
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)        
+# db_from_env = dj_database_url.config(conn_max_age=600)
+# DATABASES['default'].update(db_from_env)        
 
 
 # Password validation
